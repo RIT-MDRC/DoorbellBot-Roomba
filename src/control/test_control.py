0be68c55ab4f861e8edcc7832e9de2d8bb3e805d
DevWalrus <70732411+DevWalrus@users.noreply.github.com>
2022-10-10 23:02:44 -0400

    Added some test code and got some output

--- src/control/test_control.py
+++ src/control/test_control.py
@@ -1,18 +1,42 @@
 from pycreate2 import Create2
 import time
 
+
+def ascii_out(bot):
+    while True:
+        try:
+            bot.full()
+            bot.digit_led_ascii("1234")
+        except KeyboardInterrupt:
+            return
+
+def motor_fwd(bot, pwr):
+    while True:
+        try:
+            bot.full()
+            bot.drive_direct(pwr, pwr)
+            print(bot.get_sensors())
+        except KeyboardInterrupt:
+            return
+
+def read_sensors(bot):
+    while True:
+        try:
+            # bot.full()
+            sensors = bot.get_sensors()
+            print(sensors)
+        except KeyboardInterrupt:
+            return
+        except Exception as e:
+            print(e)
+
 def main():
-    bot = Create2("/dev/serial/by-path/pci-0000:00:14.0-usb-0:3:1.0-port0")
+    bot = Create2("/dev/serial/by-id/usb-Belkin_USB_PDA_Adapter_0109_963902-if00-port0")
     bot.start()
-    time.sleep(10)
-    bot.close()
-    #bot.full()
-    #sensors = bot.get_sensors()
-    #print(sensors)
-    #bot.drive_direct(100,100)
-    #time.sleep(2)
-    #bot.drive_stop()
-    #bot.close()
+    bot.full()
+    # ascii_out(bot)
+    # read_sensors(bot)
+    motor_fwd(bot, 100)
 
 if __name__ == "__main__":
     main()
