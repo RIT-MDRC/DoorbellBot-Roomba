43ef821f7df25d3f4e57e6efb4f1474ea5b5cc46
DevWalrus <70732411+DevWalrus@users.noreply.github.com>
2022-10-10 21:15:30 -0400

    Setup skeleton for queue code

--- src/server/routes.py
+++ src/server/routes.py
@@ -1,22 +1,23 @@
 rqueue = []
 
 def door():
-    if len(rqueue) < 2 and 'door' in rqueue:
+    if not 'door' in rqueue:
         rqueue.append('door')
         return (200, "Sending The Roomba To The Door")
-    else
+    else:
         return roomba_status()
         
 
 def elevator():
-    if len(rqueue) < 2 and 'elevator' in rqueue:
+    if not 'elevator' in rqueue:
         rqueue.append('elevator')
         return (200, "Sending The Roomba To The Elevator")
-    else
+    else:
         return roomba_status()
 
 def roomba_status():
-    return (200, "The Roomba is currently headed to the " + rqueue[0] + " and then headed to the " + rqueue[1])
+    return (200, "Status will go here")
+    #return (200, "The Roomba is currently headed to the " + rqueue[0] + " and then headed to the " + rqueue[1])
 
 def other():
     return(404, "Not sure where you're going bud")
@@ -24,5 +25,5 @@ def other():
 REQUESTS = {
     "door" : door,
     "elevator": elevator,
-    "roomba-status": roomba-status
+    "roomba_status": roomba_status
 }
