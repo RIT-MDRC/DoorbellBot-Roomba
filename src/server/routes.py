5e1c077c4c55427eba3bf70732395de10cc8e9e5
Jake Pregitzer <90400345+jwp2545@users.noreply.github.com>
2022-10-10 20:17:54 -0400

    Added status() function

--- src/server/routes.py
+++ src/server/routes.py
@@ -1,14 +1,28 @@
+rqueue = []
 
 def door():
-    return (200, "Sending The Roomba To The Door")
+    if len(rqueue) < 2 and 'door' in rqueue:
+        rqueue.append('door')
+        return (200, "Sending The Roomba To The Door")
+    else
+        return roomba_status()
+        
 
 def elevator():
-    return (200, "Sending The Roomba To The Elevator")
+    if len(rqueue) < 2 and 'elevator' in rqueue:
+        rqueue.append('elevator')
+        return (200, "Sending The Roomba To The Elevator")
+    else
+        return roomba_status()
+
+def roomba_status():
+    return (200, "The Roomba is currently headed to the " + rqueue[0] + " and then headed to the " + rqueue[1])
 
 def other():
     return(404, "Not sure where you're going bud")
 
 REQUESTS = {
     "door" : door,
-    "elevator": elevator
+    "elevator": elevator,
+    "roomba-status": roomba-status
 }
